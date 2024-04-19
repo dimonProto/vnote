b5448b2fc6890ab6ac974ee081e8b750bcbb30fa
dimonProto <dimonvkedax@gmail.com>
2024-04-19 10:54:53 +0300

    Fix getNoteTitle for non-latin symbols

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -5,7 +5,8 @@ import { Folders } from '../constants'
 
 
 export const getNoteTitle = (text: string) => {
-  const noteTitleRegEx = /[\w'?!., ]{1,50}/
+  const noteTitleRegEx = /[\p{Alpha}\p{M}\p{Nd}\p{Pc}\p{Join_C}'?!.,\s]{1,50}/gu
+  
   const noteText = text.match(noteTitleRegEx)
   return noteText ? noteText[0] : 'New note'
 }
